1d564094af8a99c492e2e2ab8df45b1126d4563a
Taka <71265675+takakv@users.noreply.github.com>
2020-10-05 01:13:00 +0300

    Get character frequencies

--- Cryptanalysis/Avig/Program.cs
+++ Cryptanalysis/Avig/Program.cs
@@ -18,14 +18,28 @@ namespace Avig
             string text;
             try
             {
-                text = File.ReadAllText(filename);
+                text = File.ReadAllText(filename).ToUpper();
             }
             catch (Exception error)
             {
                 Console.WriteLine(error.Message);
                 return;
             }
-            Console.WriteLine("Contents of {0} = {1}", filename, text);
+            Console.WriteLine("Contents of {0}:\n{1}", filename, text);
+
+            const int alphabetLength = 26;
+            var characters = new char[alphabetLength];
+            for (var i = 0; i < alphabetLength; ++i)
+                characters[i] = (char) ('A' + i);
+
+            var frequencies = new int[alphabetLength];
+            foreach (char c in text)
+                ++frequencies[Array.IndexOf(characters, c)];
+
+            Console.WriteLine();
+            Console.Write("Frequencies: ");
+            for (var i = 0; i < alphabetLength; ++i)
+                Console.Write($"{characters[i]}:{frequencies[i]} ");
         }
     }
 }
