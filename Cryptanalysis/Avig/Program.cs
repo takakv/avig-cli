e054a5be0c9ca3085f69336587ddf074a6a1ec81
Taka <71265675+takakv@users.noreply.github.com>
2020-10-05 00:55:21 +0300

    Read text from file

--- Cryptanalysis/Avig/Program.cs
+++ Cryptanalysis/Avig/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.IO;
 
 namespace Avig
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a filename with extension:");
+            string filename = Console.ReadLine();
+            string filepath = Directory.GetCurrentDirectory() + "\\" + filename;
+            if (!File.Exists(filepath))
+            {
+                Console.WriteLine("No such file.");
+                return;
+            }
+            string text;
+            try
+            {
+                text = File.ReadAllText(filename);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+                return;
+            }
+            Console.WriteLine("Contents of {0} = {1}", filename, text);
         }
     }
 }
