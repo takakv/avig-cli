ea25434487e0cd6a5a79da762a3e3ad4d9d5102e
Taka <71265675+takakv@users.noreply.github.com>
2020-10-05 00:31:49 +0300

    Create solution

---
+++ Cryptanalysis/Avig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Avig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
